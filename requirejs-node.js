252a1a2de6055858ac26bdf7548267eec6fba94e
tobisek <jiri@wix.com>
2014-08-20 22:37:00 +0300

    feature: implemented text! plugin for node

--- requirejs-node.js
+++ requirejs-node.js
@@ -4,30 +4,62 @@ var http = require("http");
 var vm = require("vm");
 
 
-function loadModuleFromHttp(moduleName, url, define) {
+function loadModuleFromHttp(moduleName, url, callback) {
     http.get(url, function (res) {
         var body = '';
         res.on('data', function(chunk) {
             body += chunk;
         });
         res.on('end', function() {
-            try {
-                requirejs.sandboxGlobals.define = define;
-                vm.runInContext(body, vm.createContext(requirejs.sandboxGlobals), moduleName);
-            }catch(err) {
-                console.error("Error while evaluating module " + moduleName + "(" + url + ")");
-                console.log(err.stack)
-            }
+            callback(null, body);
         });
         res.on('error', function (err) {
-            throw err;
+            callback(err);
         });
     });
 }
 
+function textPlugin() {
+    return {
+        load: function (moduleName, req, onload, config) {
+            var url = req.toUrl(moduleName);
+            var loader;
+
+            if (url.slice(0, 5) === "http:") {
+                loader = function (callback) {
+                    loadModuleFromHttp(moduleName, url, callback);
+                }
+            } else {
+                loader = function(callback) {
+                    fs.readFile(url, callback);
+                }
+            }
+            loader(function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    onload(result.toString());
+                }
+            });
+        }
+    }
+}
+
 function moduleLoader (moduleName, url, define) {
     if(url.slice(0,5) === "http:") {
-        loadModuleFromHttp(moduleName, url, define);
+        loadModuleFromHttp(moduleName, url, function (err, result) {
+            if(err) {
+                throw err;
+            } else {
+                try {
+                    requirejs.sandboxGlobals.define = define;
+                    vm.runInContext(body, vm.createContext(requirejs.sandboxGlobals), moduleName);
+                } catch(err) {
+                    console.error("Error while evaluating module " + moduleName + "(" + url + ")");
+                    console.log(err.stack);
+                }
+            }
+        });
     } else {
         if(moduleName.indexOf("/") === -1) {
             url = moduleName;
@@ -67,6 +99,8 @@ var requirejs = context.requirejs;
 requirejs.require = context.require;
 requirejs.define = context.define;
 requirejs.define.amd = {};
+
+requirejs.define("text", [], textPlugin);
 
 requirejs.sandboxGlobals = {
     define: requirejs.define,
